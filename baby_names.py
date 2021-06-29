c0470bc6ce1676e082204f1ec961b5d3d3b8b659
lk101101 <55768135+lk101101@users.noreply.github.com>
2021-06-29 18:27:08 -0400

    Implemented random name function

--- baby_names.py
+++ baby_names.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import random
 import matplotlib.pyplot as plt
 
 class NameReader():
@@ -36,43 +37,65 @@ def viz(year_list, rank_list, name, year):
     plt.show()
 
 def main():
-    name, gender, year = input("Find out how many babies are named a certain name in a specific year. Format as 'name gender (m/f) year': ").split()
+    answer = input("Select an option: r to return a random name or p to return the popularity ranking of a name: ")
 
-    current_file = 'yob2020.txt'
-    past_file = 'yob' + year + '.txt'
+    if answer.lower() == 'p':
+        name, gender, year = input("Find out how many babies are named a certain name in a specific year. Format as 'name gender (m/f) year': ").split()
 
-    full_gender = ""
-    if (gender.lower() == 'f'):
-        full_gender = "girls'"
-    else:
-        full_gender = "boys'"
+        current_file = 'yob2020.txt'
+        past_file = 'yob' + year + '.txt'
+
+        full_gender = ""
+        if (gender.lower() == 'f'):
+            full_gender = "girls'"
+        else:
+            full_gender = "boys'"
     
-    # create instances of NameReader class
-    current_name = NameReader(current_file, name, gender)
-    past_name = NameReader(past_file, name, gender)
+        # create instances of NameReader class
+        current_name = NameReader(current_file, name, gender)
+        past_name = NameReader(past_file, name, gender)
     
-    # get ranks from current and specified year
-    cur_rank = current_name.ranking_of_name()
-    past_rank = past_name.ranking_of_name()
+        # get ranks from current and specified year
+        cur_rank = current_name.ranking_of_name()
+        past_rank = past_name.ranking_of_name()
 
-    print("The {} name {} was used {} times in 2020 and {} times in {}".format(full_gender, name.upper(), cur_rank, past_rank, year))
+        print("The {} name {} was used {} times in 2020 and {} times in {}".format(full_gender, name.upper(), cur_rank, past_rank, year))
 
-    # make chart to display change in rank of name over time 
-    years = []
-    ranks = []
-    for i in range(int(year), 2021):
-        years.append(str(i))
-        new_file = 'yob' + str(i) + '.txt'
-        new_name = NameReader(new_file, name, gender)
-        new_rank = new_name.ranking_of_name()
+        # visualization code
+
+        years = []
+        ranks = []
+        # gather the ranks for each year between the specified and current year
+        for i in range(int(year), 2021):
+            years.append(str(i))
+            new_file = 'yob' + str(i) + '.txt'
+            new_name = NameReader(new_file, name, gender)
+            new_rank = new_name.ranking_of_name()
+
+        # if name isn't in file, use 0 as rank
         if (new_rank == None):
             ranks.append(0)
         else:
             ranks.append(int(new_rank))
 
-    viz(years, ranks, name, year)
+        viz(years, ranks, name, year)
 
+    # random name generator
+    else:
+        # TO DO: 
+            # implement way to specify gender and rank
 
+        # get random year + file
+        year = random.randrange(1880, 2020)
+        file_name = 'yob' + str(year) + '.txt'
+
+        # get random line
+        line = random.choice(open(file_name).readlines())
+        # split line into words
+        line = line.split(',')
+
+        # print year + name
+        print(str(year) + " : " + line[0])
     
 if __name__ == '__main__':
     main()
