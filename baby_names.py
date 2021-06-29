d277699d66503408b47acb092dfaa3928fbd77d7
lk101101 <55768135+lk101101@users.noreply.github.com>
2021-06-28 20:29:16 -0400

    added gender option

--- baby_names.py
+++ baby_names.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 class NameReader():
-    def __init__(self, filename, name):
+    def __init__(self, filename, name, gender):
         self.base_path = os.path.abspath(os.path.dirname(__file__))
         self.full_path = os.path.join(self.base_path, filename)
         self.file_obj = open(self.full_path, 'r')
@@ -10,29 +10,40 @@ class NameReader():
         self.file_obj.close()
 
         self.name = name
+        self.gender = gender
 
     # iterate through list
     # if specified name matches a name from the list, return its ranking
     def ranking_of_name(self):
         for i in self.raw_data[1:]:
             separated = i.split(',')
-            if separated[0].lower() == self.name.lower():
-                return separated[2].strip('\n')
+            if separated[1].lower() == self.gender.lower() and separated[0].lower() == self.name.lower():
+                    return separated[2].strip('\n')
 
 def main():
-    name, year = input("Find out how many babies are named a certain name in a specific year. Format as 'name year': ").split()
+    name, gender, year = input("Find out how many babies are named a certain name in a specific year. Format as 'name gender (m/f) year': ").split()
 
     current_file = 'yob2020.txt'
     past_file = 'yob' + year + '.txt'
+
+    full_gender = ""
+    if (gender.lower() == 'f'):
+        full_gender = "girls'"
+    else:
+        full_gender = "boys'"
     
     # create instances of NameReader class
-    current_name = NameReader(current_file, name)
-    past_name = NameReader(past_file, name)
+
+    current_name = NameReader(current_file, name, gender)
+    past_name = NameReader(past_file, name, gender)
     # get ranks from current and specified year
     cur_rank = current_name.ranking_of_name()
     past_rank = past_name.ranking_of_name()
 
-    print("The name {} was used {} times in 2020 and {} times in {}".format(name.upper(), cur_rank, past_rank, year))
+    print("The {} name {} was used {} times in 2020 and {} times in {}".format(full_gender, name.upper(), cur_rank, past_rank, year))
+
+
+
     
 if __name__ == '__main__':
     main()
