64df5d17ac07eb92e2001b65c257d1519fca6f4e
laraking <laraking@umich.edu>
2023-06-04 21:58:08 -0400

    Separate random name generation code into separate functions

--- baby_names.py
+++ baby_names.py
@@ -5,8 +5,8 @@ import codecs
 import random
 import matplotlib.pyplot as plt
 import argparse
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 # read + return raw data from files
 def data_func(filename):
@@ -103,96 +103,70 @@ f 2 s   -> 2 random first names of specified gender (female) and two random last
 # Parses command line inputs for random name generator and calls specific functions
 def random_name_generator():
     rand_input = input("Specify a gender (m/f) and/or number of names to generate (>1) and/or whether to include a random surname (s). Leave blank for a single random name: ")
-    surname = False
-    first_name = False
-    specified_gender = False
-    gender = 'x'
-    
-    # ** nothing specified
-    if not rand_input:
-        first_name = True
-        random_name(first_name, specified_gender, gender, surname)
+    parts = rand_input.split(' ')
+    length = len(parts)
+        
+    if parts == ['']:
+        random_name(True, False, False, False)
+    elif length == 1:
+        handle_single_input(parts[0])
+    elif length == 2:
+        handle_two_inputs(parts[0], parts[1])
+    elif length == 3:
+        handle_three_inputs(parts[0], parts[1], parts[2])
     else:
-        rand_input = rand_input.split(' ')
-        length = len(rand_input)
-
-        # ** all 3 inputs provided - gender, number and surname
-        if length == 3 and rand_input[2] == 's':
-            gender = rand_input[0]
-            number = int(rand_input[1])
-            surname = True
-            specified_gender = True
-            first_name = True
+        print("\nERROR: You may have provided too many inputs (>3) or one of your inputs is invalid.\nRemember to specify a gender (m/f), use digits to print a certain number of names, or use the letter s to generate a random surname.\n")
+        
+        
+def handle_single_input(input):
+    if input == 's':
+        sur = random_surname()
+        print(sur)
+    elif input.isnumeric():
+        number = int(input)
+        for num in range(0, number):
+            random_name(True, False, False, False)
+    elif input == 'f' or input == 'm':
+        random_name(True, True, input, False)
+    else:
+        print("\nERROR: You provided a single invalid input.\nRemember to specify a gender (m/f), use digits to specify the number of names, or use the letter s to generate a random surname.\n")
+        
+def handle_two_inputs(input1, input2):
+    if input1.isnumeric() and input2 == 's':
+        number = int(input1)
+        for num in range(number):
+            sur = random_surname()
+            print(sur)
+    elif input1 == 'f' or input1 == 'm':
+        gender = input1
+        if input2.isnumeric():
+            number = int(input2)
             for num in range(0, number):
-                random_name(first_name, specified_gender, gender, surname)
-
-        # ** 2 inputs provided
-        # determine if number + surname, gender + surname, or gender + number
-        elif length == 2:
-            first = rand_input[0]
-            second = rand_input[1]
-
-            # number and surname
-            if (first.isnumeric() and second == 's'):
-                number = int(first)
-                surname = True
-                for num in range(0, number):
-                    sur = random_surname()
-                    print(sur)
-
-            # gender specified
-            elif (first == 'f' or first == 'm'):
-                gender = first
-                first_name = True
-                specified_gender = True
-                # gender and number
-                if (second.isnumeric()):
-                    number = int(second)
-                    for num in range(0, number):
-                        # random_specific_gender(gender, surname)
-                        random_name(first_name, specified_gender, gender, surname)
-                # gender and surname
-                elif (second == 's'):
-                    surname = True
-                    specified_gender = True
-                    random_name(first_name, specified_gender, gender, surname)
-                # ERROR: gender is specified, but invalid second input
-                else:
-                    print("\nERROR: You specified gender (m/f) but your second input is invalid.\nInput a digit x (>1) to generate x number of names or s to generate a random surname.\n")
-            # ERROR
-            else:
-                print("\nERROR: You provided at least one invalid input.\nRemember to specify a gender (m/f), use digits to print a certain number of names, or use the letter s to generate a random surname.\n")
-
-        # ** one input
-        elif length == 1:
-            first = rand_input[0]
-            # gender
-            if (first == 'f' or first == 'm'):
-                first_name = True
-                specified_gender = True
-                random_name(first_name, specified_gender, first, surname)
-            # number
-            elif first.isnumeric():
-                first_name = True
-                number = int(first)
-                for num in range(0, number):
-                    random_name(first_name, specified_gender, gender, surname)
-            # surname
-            elif (first == 's'):
-                surname = True
-                sur = random_surname()
-                print(sur)
-            # ERROR
-            else:
-                print("\nERROR: You provided a single invalid input.\nRemember to specify a gender (m/f), use digits to print a certain number of names, or use the letter s to generate a random surname.\n")
+                random_name(True, True, gender, False)
+        elif input2 == 's':
+            random_name(True, True, gender, True)
         else:
-            print("\nERROR: You may have provided too many inputs (>3) or one of your inputs is invalid.\nRemember to specify a gender (m/f), use digits to print a certain number of names, or use the letter s to generate a random surname.\n")
+            print("\nERROR: You specified gender (m/f) but your second input is invalid.\nInput a digit x (>1) to generate x number of names or s to generate a random surname.\n")
+    else:
+        print("\nERROR: You provided at least one invalid input.\nRemember to specify a gender (m/f), use digits to specify the number of names, or use the letter s to generate a random surname.\n")
+
+def handle_three_inputs(input1, input2, input3):
+    if input3 == 's':
+        gender = input1
+        number = int(input2)
+        for num in range(0, number):
+            random_name(True, True, gender, True)
+    else:
+        print("\nERROR: You may have provided too many inputs (>3) or one of your inputs is invalid.\nRemember to specify a gender (m/f), use digits to print a certain number of names, or use the letter s to generate a random surname.\n")
 
 
 # * Gets a random surname from the surname CSV file
 def random_surname():
     with open("2010CensusSurnames.csv") as f:
         reader = csv.reader(f)
+        # skip first two rows in file
+        next(reader)
+        next(reader)
         surnames = list(reader)
         random_line = random.choice(surnames)
         sur = random_line[0].lower()
@@ -299,19 +273,19 @@ def fav_names_file():
         print("Saved!")
 
 
+options = {
+    'p': popularity,
+    'r': random_name_generator,
+    'm': name_meaning,
+    's': fav_names_file
+}
 
 def main():
     answer = input("Select an option: r to return a random name, p to return the popularity ranking of a name, m to return details about a name, or s to save a favorite name: ")
-    if answer.lower() == 'p':
-        popularity()
-    elif answer.lower() == 'r':
-        random_name_generator()
-    elif answer.lower() == 'm':
-        name_meaning()
-    elif answer.lower() == 's':
-        fav_names_file()
+    if answer in options:
+        options[answer]()
     else:
-        print("Invalid choice.\nChoose r for a random name, p for a name's popularity, m for a name's meaning, or s to save or print favorite names.")
+        print("Invalid choice.\nChoose r for a random name, p for a name's popularity, m for a name's meaning, or s to save your favorite names.")
 
 if __name__ == '__main__':
     main()
