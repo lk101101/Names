a1632b881bb78fba3ba38b2b2159ef4d5f196dd2
laraking <laraking@umich.edu>
2024-05-21 20:32:55 -0400

    Develop Flask code

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, render_template
+import names
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """
+    Serve the home page. 
+    """
+    return render_template('index.html')
+
+# TODO - Route for popularity function
+
+# @app.route('/popularity', methods=['GET', 'POST'])
+# def get_popularity():
+#     # Assume that you have a `popularity` function in your 'names' module. It should take name, gender, year, and return a plot URL.
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         gender = request.form['gender']
+#         year = request.form['year']
+#         # Implement the 'popularity' function in your 'names' module.
+#         plot_url = names.popularity(name, gender, year)
+#         return render_template('popularity.html', plot_url=plot_url)
+#     return render_template('popularity_form.html')
+
+
+@app.route('/random_name', methods=['GET', 'POST'])
+def random_name():
+    """
+    Route to generate random names.
+
+    Output: 
+    - Rendered 'generate_form.html' template with a list of generated names when POST method is used.
+    - Rendered 'generate_form.html' template with form when GET method is used.
+    """
+    if request.method == 'POST':
+        gender = request.form.get('gender', default='')
+        num_names = int(request.form.get('num_names', default=1))
+        include_surname = request.form.get('surname') == 'yes'
+        cur_names = [names.random_name(gender, include_surname)
+                     for _ in range(num_names)]
+        return render_template('generate_form.html', names=cur_names)
+    return render_template('generate_form.html')
+
+
+@app.route('/name_information', methods=['GET', 'POST'])
+def name_information():
+    """
+    Route to retrieve information about a name. 
+
+    Output:
+    - Rendered 'name_info.html' template with name information when POST method is used.
+    - Rendered 'name_info.html' template with form when GET method is used.
+    """
+    if request.method == 'POST':
+        name = request.form.get('name', '')
+        gender = request.form.get('gender', '')
+
+        cur_text = names.name_information(name, gender)
+        print(cur_text)
+
+        return render_template('name_info.html', texts=cur_text)
+    return render_template('name_info.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
