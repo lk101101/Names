fa38721c8bd885ebfeeaf0a165c2a7fe6e07ddbf
laraking <laraking@umich.edu>
2024-06-02 00:06:25 -0400

    Remove unused imports

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, session
-import altair as alt
+from flask import Flask, request, render_template
 import names
 import visualizations
 
